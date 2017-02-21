c743b30615a966202fbab62f4c396edcb06ca799
links234 <murtaza_alexandru73@yahoo.com>
2017-02-21 10:29:53 -0800

    Initial commit

---
+++ source/redis/RedisConnectionPool.js6.js
@@ -0,0 +1,44 @@
+var redis = require("redis");
+
+var redisConnectionPool = new Map();
+var redisSharedConnectionPool = new Map();
+
+class RedisConnectionPool {
+    constructor() {
+    }
+
+    static getConnection(redisAddress) {
+        if (!redisConnectionPool.has(redisAddress)) {
+            redisConnectionPool.set(redisAddress, new Set());
+        }
+
+        let redisClient = redis.createClient(redisAddress);
+
+        let redisClients = redisConnectionPool.get(redisAddress);
+        redisClients.add(redisClient);
+        redisConnectionPool.set(redisAddress, redisClients);
+
+        return redisClient;
+    }
+
+    static getSharedConnection(redisAddress) {
+        if (!redisSharedConnectionPool.has(redisAddress)) {
+            redisSharedConnectionPool.set(redisAddress, redis.createClient(redisAddress));
+        }
+
+        return redisSharedConnectionPool.get(redisAddress);
+    }
+
+    static quit() {
+        for (let [address, connections] of redisConnectionPool) {
+            for (let connection of connections) {
+                connection.quit();
+            }
+        }
+        for (let [address, connection] of redisSharedConnectionPool) {
+            connection.quit();
+        }
+    }
+}
+
+module.exports = RedisConnectionPool;
